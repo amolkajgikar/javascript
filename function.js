f4615826633c40a09236b3866cd669563a5413c8
Amol Kajgikar <amolkajgikar@gmail.com>
2021-11-27 22:36:22 +0400

    initial version

---
+++ function.js
@@ -0,0 +1,2 @@
+//function syntax
+//function name (parameter1, parameter2) {code to be executed}
---
+++ prototype.js
@@ -0,0 +1,24 @@
+
+ function Student(name, age, course) {
+    this.name = name;
+    this.age = age;
+    this.course = course;
+}
+Student.prototype.add = function() {
+    console.log('ADDt')
+}
+Student.prototype.get = function() {
+    console.log('GET')
+}
+Student.prototype.update = function() {
+    console.log('UPDATE')
+}
+Student.prototype.delete = function() {
+    console.log('DELETE')
+}
+var std = new Student("std01", 33, "Mean");
+console.log(std);
+std.add();
+std.get();
+std.update();
+std.delete();
