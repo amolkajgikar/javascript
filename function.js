b231be78c3b89642ca4316c0e5433738e78ffc40
Amol Kajgikar <amolkajgikar@gmail.com>
2021-11-28 16:38:58 +0400

    version

--- function.js
+++ function.js
@@ -1,2 +1,10 @@
 //function syntax
 //function name (parameter1, parameter2) {code to be executed}
+function sum(num1,num2){
+    console.log(num1+num2);
+}
+// we can store result in constant or variable as give below
+//const result=sum(10,30);
+var result=sum(10,40);
+
+//console.log(result);
